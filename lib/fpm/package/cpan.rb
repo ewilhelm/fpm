530f129e1793924d54453dac1ccb34cf61620c75
Eric Wilhelm <ewilhelm@sourcefire.com>
2013-01-24 18:13:26 -0800

    lib/fpm/package/cpan.rb - first crack at basic functionality

---
+++ lib/fpm/package/cpan.rb
@@ -0,0 +1,64 @@
+require "fpm/package"
+require "json"
+require "find"
+
+# Premise: Sidestep all of the historical accidents and just require
+# that we have a MYMETA.json v2+ -- for now, just start from there and
+# add the download+make+(test?) business later (or not.)
+
+class FPM::Package::CPAN < FPM::Package
+
+  def input(package)
+    path = package # TODO later: download + make
+    raise "is not a directory" unless File.directory?(path)
+
+    mymetajson = File.join(path, 'MYMETA.json')
+    raise "must have MYMETA.json file #{metajson}" unless
+      File.exists?(mymetajson)
+
+    load_package_info(mymetajson)
+
+    install_to_staging(path)
+  end
+
+  def load_package_info(mymetajson)
+    i = JSON::parse(File.open(mymetajson).readlines.join(''))
+
+    raise "must have modern meta-spec" unless i['meta-spec'] &&
+      (i['meta-spec']['version'].to_f >= 2)
+
+    # arch check for bs files (beats searching for so/dylib/dll/...)
+    found_bs = 0
+    archdir = File.expand_path('../blib/arch/auto', mymetajson)
+    if File.exists?(archdir)
+      ::Find.find(archdir) {|p|
+        if p =~ /\.bs$/
+          Find.prune
+          found_bs += 1
+        end
+      }
+    end
+    self.architecture = found_bs > 0 ? 'native' : 'all'
+
+
+
+  end
+
+  def install_to_staging(path)
+    # XXX the installbase or such is assumed to have been correct here
+    ::Dir.chdir(path) {
+      run = File.exists?('Build') ?
+          ['./Build', 'install', '--destdir', staging_path]
+        : File.exists?('Makefile') ?
+          ['make', 'install', 'DESTDIR=' + staging_path]
+        : (raise "no build / make artifacts found")
+      safesystem(*run);
+
+      # TODO just Find.find the perllocal.pod files?
+      ::Dir.glob(staging_path + '/usr/lib/perl/*/perllocal.pod').each {|f|
+        ::File.unlink(f)
+      }
+    }
+  end
+
+end
