91ea842f35c67088243635452b9ed5e0b5370f0f
Eric Wilhelm <ewilhelm@sourcefire.com>
2014-03-16 09:59:27 -0700

    lib/fpm/package/pkgin.rb - re-format args

--- lib/fpm/package/pkgin.rb
+++ lib/fpm/package/pkgin.rb
@@ -22,7 +22,15 @@ class FPM::Package::Pkgin < FPM::Package
 
     File.write(build_path("description"), self.description + "\n")
 
-    args = [ "-B", build_path("build-info"), "-c", build_path("comment"), "-d", build_path("description"), "-f", build_path("packlist"), "-I", "/opt/local", "-p", staging_path,  "-U", "#{cwd}/#{name}-#{self.version}-#{iteration}.tgz" ]
+    args = [
+      "-B", build_path("build-info"),
+      "-c", build_path("comment"),
+      "-d", build_path("description"),
+      "-f", build_path("packlist"),
+      "-I", "/opt/local",
+      "-p", staging_path, 
+      "-U", "#{cwd}/#{name}-#{self.version}-#{iteration}.tgz",
+    ]
     safesystem("pkg_create", *args)
 
   end
